12b7e86a9c9792f0313d42a8cab4b825abef9b23
Ondrej Sika <ondrej@ondrejsika.com>
2020-05-18 19:09:53 +0200

    Pipedrive - add __getattr__ to simple request calls like Pipedrive().get_deals(), bump version v0.2.0

--- pipedrive/__init__.py
+++ pipedrive/__init__.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from urllib.parse import urlencode
 
 GET = "GET"
 POST = "POST"
@@ -18,10 +19,38 @@ class Pipedrive(object):
         self._origin = "https://app.pipedrive.com"
 
     def request(self, method, path, data={}):
+        url = self._origin + path + "?api_token=%s" % self._token
+        if method == GET and data:
+            if data:
+                url += "&" + urlencode(data)
         r = requests.request(
             method,
-            self._origin + path + "?api_token=%s" % self._token,
+            url,
             data=json.dumps(data),
             headers={"Content-Type": "application/json"},
         )
         return r.json()
+
+    def __getattr__(self, name):
+        def wrapper(data={}):
+            action, raw_path = name.split("_", 1)
+            method = {"get": GET, "create": POST, "update": PUT, "delete": DELETE}[
+                action
+            ]
+            path = raw_path.replace("_", "/")
+            if method == PUT:
+                id = data.pop("id")
+                path = "%s/%d" % (path, id)
+            r_data = self.request(method, "/v1/" + path, data)
+            if "error" in r_data:
+                raise self.Error(r_data)
+            return r_data
+
+        return wrapper
+
+    class Error(Exception):
+        def __init__(self, response):
+            self.response = response
+
+        def __str__(self):
+            return self.response.get("error", "No error provided")
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pipedrive",
-    version="0.1.0",
+    version="0.2.0",
     author="Ondrej Sika",
     author_email="ondrej@ondrejsika.com",
     description="Pipedrive Client",
